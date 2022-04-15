a0e9d4485d67ae68f5ea864d4ce9bcbead08401b
Chetan <viranichetan666@gmail.com>
2022-04-15 09:57:08 +0530

    login page design complete

--- src/components/Login.jsx
+++ src/components/Login.jsx
@@ -6,11 +6,44 @@ const { login } = authAction;
 
 const Login = () => {
   const dispatch = useDispatch();
+  const [formData, setFormData] = useState({ email: "", password: "" });
+
+  const handleLogin = (e) => {
+    e.preventDefault();
+    if (!formData.email || !formData.password) {
+      alert("enter valid details");
+    }
+    dispatch(login({ ...formData }));
+  };
+
+  const handleOnChange = (e) =>
+    setFormData((prev) => ({
+      ...prev,
+      [e.target.name]: e.target.value,
+    }));
 
   return (
-    <div>
-      Login Page
-      <button onClick={() => dispatch(login("something"))}>login</button>
+    <div className="Login__container">
+      <div className="Login__wrapper">
+        <div className="Login__header">What TODO?</div>
+        <form className="Login__form" onSubmit={handleLogin}>
+          <input
+            type="email"
+            name="email"
+            placeholder="Email Address"
+            onChange={handleOnChange}
+            value={formData.email}
+          />
+          <input
+            name="password"
+            type="password"
+            placeholder="Password"
+            onChange={handleOnChange}
+            value={formData.password}
+          />
+          <button type="submit">Click to Login</button>
+        </form>
+      </div>
     </div>
   );
 };
